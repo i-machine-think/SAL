b50ac47cf1f0241c35b0357085bcbd3430c8c372
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-06-16 16:39:30 +0200

    Update parity test

--- example.sh
+++ example.sh
@@ -5,14 +5,14 @@ DEV_PATH='./test_parity.tsv'
 EXPT_DIR=example
 
 # set values
-EMB_SIZE=128
-H_SIZE=128
+EMB_SIZE=32
+H_SIZE=32
 N_LAYERS=1
 CELL='lstm'
-EPOCH=10
+EPOCH=1000
 PRINT_EVERY=100
 TF=0.5
 
 # Start training
 echo "Train model on example data"
-python train_model.py --max_len 100 --train $TRAIN_PATH --output_dir $EXPT_DIR --print_every $PRINT_EVERY --embedding_size $EMB_SIZE --hidden_size $H_SIZE --rnn_cell $CELL --n_layers $N_LAYERS --epoch $EPOCH --print_every $PRINT_EVERY --teacher_forcing $TF --attention 'pre-rnn' --attention_method 'mlp' --ponder_encoder --max_ponder_steps 100 --ponder_epsilon 0.01 --ponder_penalty_scale 0.0001 --batch_size 32 --eval_batch_size 1000
+python train_model.py --max_len 100 --train $TRAIN_PATH --output_dir $EXPT_DIR --print_every $PRINT_EVERY --embedding_size $EMB_SIZE --hidden_size $H_SIZE --rnn_cell $CELL --n_layers $N_LAYERS --epoch $EPOCH --print_every $PRINT_EVERY --teacher_forcing $TF --ponder_encoder --ponder_decoder --max_ponder_steps 10 --ponder_epsilon 0.05 --ponder_penalty_scale 0.001 --batch_size 100 --eval_batch_size 1000 --ignore_output_eos
--- generate_parity.py
+++ generate_parity.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-sequence_length = 64
-n_examples = 10000
-output_file = open('train_parity.tsv', 'w')
+def gen(sequence_length, n_examples, output_file):
+    for i in range(n_examples):
+        x = np.random.randint(3, size=(sequence_length)) - 1
+        unique, counts = np.unique(x, return_counts=True)
 
-for i in range(n_examples):
-    x = np.random.randint(3, size=(sequence_length)) - 1
-    unique, counts = np.unique(x, return_counts=True)
+        try:
+            y = dict(zip(unique, counts))[1] % 2
+        except Exception:
+            y = 0
 
-    try:
-        y = dict(zip(unique, counts))[1] % 2
-    except Exception:
-        y = 0
+        x = " ".join([str(i) for i in x])
 
-    x = " ".join([str(i) for i in x])
+        output_file.write("{}\t{}\n".format(x, y))
 
-    output_file.write("{}\t{}\n".format(x, y))
+gen(sequence_length=4, n_examples=10000, output_file=open('train_parity.tsv', 'w'))
+gen(sequence_length=4, n_examples=100, output_file=open('test_parity.tsv', 'w'))
